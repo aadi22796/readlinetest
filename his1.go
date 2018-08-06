d2bc38e1f4848112c4ec7cbd5ae36a149eb93e52
aadi22796 <aadi22796@aadi22796@gmail.com>
2018-08-06 13:15:25 -0500

    updated 1:15PM, commit2

--- his1.go
+++ his1.go
@@ -4,7 +4,9 @@ import (
     "fmt"
     "os"
     	"bufio"
-	)
+	"sync"
+	"syscall"
+)
 
 func write(fil *os.File){
 	fmt.Println("Enter your string")
@@ -20,12 +22,34 @@ func write(fil *os.File){
 		write(fil)
 	}
 	return
-}
-func main() {
-	f, err := os.OpenFile("data", os.O_APPEND|os.O_WRONLY, 0644)
+	}
+
+func fopen(filename string, mode int, perm os.FileMode, wg sync.WaitGroup, m sync.Mutex){
+	m.Lock()
+	f, err := os.OpenFile(filename, mode, perm)
+	lockerr:=syscall.Flock(int(f.Fd()), 0600)
+	if lockerr!=nil{
+		panic(lockerr)
+	}
 	if err!=nil{
 		panic(err)
-		}
+	}
 	defer f.Close()
 	write(f)
+	m.Unlock()
+	wg.Done()
+	lockerr2:=syscall.Flock(int(f.Fd()), 0644)
+	if lockerr2!=nil{
+		panic(lockerr2)
+	}
 }
+
+func main() {
+	var w sync.WaitGroup
+	var m sync.Mutex
+	w.Add(1)
+	go fopen("data", os.O_APPEND|os.O_WRONLY, 0644, w, m)
+	w.Wait()
+}
+
+//f, err := os.OpenFile("data", O_APPEND|O_WRONLY, 0644)
